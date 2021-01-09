33189fab7c5a4cb516d49c07802a382f28ae3cf1
Bartosz Bonczak <bjb417@ad.nyu.edu>
2021-01-08 19:52:35 -0500

    aded scripts for hdfs data processing

---
+++ scripts/data_processing/01_mobility_data_aggregation.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" 
+Author: Bartosz Bonczak
+Title: 01_mobility_data_aggregation
+
+Description:
+This code is designed to simplify and reduce both spatial and temporal resolution of the raw, annonymized mobility data provided by VenPath Inc. in order further reduce the risk of re-identification of the individual device and to normalize ping frequency in various application types. It is achieved by the following steps:
+- standardize device's uniqe and annonymous adevrtisement ID (ad_id) information by using upper case 
+- aggregating mobility data by 5-minute interval and reporting  average latitude and longitude
+"""
+
+# Imports
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+
+from pyspark.sql.functions import mean, window, upper, col, count, round, collect_set
+from pyspark.sql.types import * # import types
+
+
+if __name__ == "__main__":
+	sc = SparkContext()
+	sqlContext = SQLContext(sc)
+
+	fs_path = ""   # specify path to the data
+	dest_path = "" # specify output path
+
+	# Specify schema
+	schema = StructType([
+		StructField("id", LongType(), True),
+		StructField("app_id", StringType(), True),
+		StructField("ad_id", StringType(), True),
+		StructField("id_type", StringType(), True),
+		StructField("country_code", StringType(), True),
+		StructField("device_make", StringType(), True),
+		StructField("device_model", StringType(), True),
+		StructField("device_os", StringType(), True),
+		StructField("device_os_version", StringType(), True),
+		StructField("latitude", DoubleType(), True),
+		StructField("longitude", DoubleType(), True),
+		StructField("timestamp", TimestampType(), True),
+		StructField("ip_address", StringType(), True),
+		StructField("horizontal_acc", DoubleType(), True),
+		StructField("vertical_acc", StringType(), True),
+		StructField("foreground", BooleanType(), True),
+	])
+
+	# load data
+	df = sqlContext.read.format("csv") \
+		.schema(schema) \
+		.option("header", "false") \
+		.load(fs_path)
+
+	# standardize ad_id 
+	df = df.withColumn('ad_id_upper', upper(col('ad_id')))
+		
+	# aggregate data
+	agg = df.groupby(
+		window("timestamp", "5 minutes"), "ad_id_upper").agg( \
+			mean("longitude").alias("lon_avg"), \
+			mean("latitude").alias("lat_avg"), \
+			count("id").alias("ping_counts"), \
+			collect_set("country_code")[0].alias("country"), \
+			collect_set("device_make")[0].alias("make")
+	)
+
+	# save to parquet files
+	agg.select(
+		agg.window.start.cast("string").alias("time_start"), \
+		agg.window.start.cast("date").alias("date"),\
+		col("ad_id_upper").alias("ad_id"), \
+		round(col("lon_avg"), 6).alias("lon_avg"), \
+		round(col("lat_avg"), 6).alias("lat_avg"), \
+		"ping_counts", "country", "make") \
+	.write.partitionBy("date") \
+	.mode("overwrite") \
+	.parquet("{}/daily_5min_agg_parquet".format(dest_path))			
---
+++ scripts/data_processing/02_subset_houston_activity.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" 
+Author: Bartosz Bonczak
+Title: 02_subset_houston_activity
+
+Description:
+This code snippet generates a subset of mobility data within the Houston, TX area defined by a bounding box and covering two months period of August and September 2017.
+"""
+
+## Imports
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark import SparkFiles
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import * # import types
+
+if __name__ == "__main__":
+	sc = SparkContext()
+	sqlContext = SQLContext(sc)
+
+	fs_path = ""   # specify data path
+	dest_path = "" # specify output destination path
+
+	# load aggregated data
+	df = sqlContext.read.parquet('{}/daily_5min_agg_parquet/'.format(fs_path))
+
+	# filter data to represent pings from August and September 2017 
+	# within the Houston area and remove randomized devices
+	subset = df.filter(
+		(col('latitude')>=-96.0266) \
+		& (col('latitude')<=-94.8285) \
+		& (col('longitude')>=29.4492) \
+		& (col('longitude')<=30.2409) \
+		& (col('year')==2017) \
+		& (col('month').isin([8,9]) )
+		& (col('ad_id')!='00000000-0000-0000-0000-000000000000') \
+	)
+
+	# save as compressed CSV files for further processing
+	subset.select("time", "ad_id", "latitude", "longitude", "count") \
+	.coalesce(200).write \
+	.format("com.databricks.spark.csv") \
+    .option("codec", "org.apache.hadoop.io.compress.GzipCodec") \
+    .save("{}/houston_aug_sep_2017/".format(dest_path))
