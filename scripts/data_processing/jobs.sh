582dee3c4ea26e2366c0276520a58229079e13c8
Bartosz Bonczak <bjb417@ad.nyu.edu>
2021-01-11 13:27:26 -0500

    Added DBSCAN and evacuation analysis

--- scripts/data_processing/jobs.sh
+++ scripts/data_processing/jobs.sh
@@ -4,3 +4,6 @@ spark-submit --num-executors 200 --executor-memory 4g --driver-memory 16g --conf
 spark-submit --num-executors 200 --executor-memory 4g --driver-memory 16g --conf spark.yarn.maxAppAttempts=1 --conf spark.executor.memoryOverhead=2g 02_subset_houston_activity.py
 hdfs dfs -get <dest_path>/houston_aug_sep_2017 ../../data/
 python 03_gzip2csv_houston_data.py
+spark-submit --num-executors 200 --executor-memory 4g --driver-memory 16g --conf spark.yarn.maxAppAttempts=1 --conf spark.executor.memoryOverhead=2g 04_DBSCAN_clustering_for_top25%_users.py
+hdfs dfs -get <dest_path>/DBSCAN_clustering ../../data/
+python 05_gzip2csv_clusters_data.py
